05bbc5becc8286e939f7e725a9a287448c707f6f
Magnan <amathmagnan@gmail.com>
2021-03-29 18:27:29 +0200

    issues model address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_155836) do
+ActiveRecord::Schema.define(version: 2021_03_29_160934) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
