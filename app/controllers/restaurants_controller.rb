3221a9a3ee454f77a88b91f47a98912fc7c7285d
Magnan <amathmagnan@gmail.com>
2021-04-01 14:59:40 +0200

    fin exo crud lewagon

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,22 +14,38 @@ class RestaurantsController < ApplicationController
 
 #pour afficher le formulaire
   def new
+    @restaurant = Restaurant.new
   end
 
 #pour créer
   def create
+    Restaurant.create(params[:restaurant])
+    redirect_to restaurant_path
   end
 
 #pour un formulaire d'édition
   def edit
+    @restaurant = @Restaurant.find(params[:id])
   end
 
 #pour intercepter la requete
-  def update
-  end
+def update
+   @restaurant = @Restaurant.find(params[:id])
+   @restaurant.update(restaurant_params)
+   redirect_to restaurants_path
+end
 
 #pour supprimer
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurant_path
+  end
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :stars)
   end
 
 end
